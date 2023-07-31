b7470ef0d240010b289c9f9f70b765a201a8ed8c
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-07-31 23:31:47 +0300

    Solution Task 2

--- src/app/task2/Main.java
+++ src/app/task2/Main.java
@@ -1,7 +1,30 @@
 package app.task2;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Queue<String> products = getInitData();
+        getOutput("Initial data:", products);
+        updateData(products);
+        getOutput("Updated data:", products);
+    }
+
+    public static Queue<String> getInitData() {
+        return new LinkedList<>(List.of("apricot", "mango", "cherry", "rice"));
+    }
+
+    public static void updateData(Queue<String> products) {
+        products.poll();
+    }
+
+    public static void getOutput(String title, Queue<String> products) {
+        System.out.println(title);
+        AtomicInteger counter = new AtomicInteger(1);
+        products.forEach(product -> System.out.println(counter.getAndIncrement() + ") " + product));
+        System.out.println();
     }
 }
